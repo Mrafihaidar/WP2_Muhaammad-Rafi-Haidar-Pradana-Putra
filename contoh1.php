c8cc85006f3c0e622c4f353dc2593cec91e3304a
Mrafihaidar <90681157+Mrafihaidar@users.noreply.github.com>
2021-09-21 22:40:59 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+            echo "<h1>perkenalkan</h1>";
+            echo"nama saya muhammad rafi haidar pradana putra saya tinggal di daerah cibarusah olahraga yg disukai futsal dan bermain bola";
+    }
+}
